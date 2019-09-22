33426e2e3dd8358c242864b5abdf42c7a917dd60
nicky-code <aline.nicole7@gmail.com>
2019-09-22 22:10:10 +0200

    created the auth folder and inside it created other files.

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,17 +1,20 @@
 import unittest
 from app.models import User
 
-class UserTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the User class
-    '''
+
+class UserModelTest(unittest.TestCase):
 
     def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_user = User()
+        self.new_user = User(password = 'mangoe')
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_user,User))
-        
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+    
+    def test_no_access_password(self): 
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('mangoe'))  
+            
+            
