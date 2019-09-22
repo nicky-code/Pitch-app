fec6bc964272df8eab180d4e25e4b77db64c7e95
nicky-code <aline.nicole7@gmail.com>
2019-09-23 00:45:47 +0200

    so many changes have made.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,20 @@
-from flask import render_template
+from flask import render_template,abort
 from . import main
+from flask_login import login_required
+from ..models import User
 
 # Views
 # @main.route('/')
 # def index():
     #....
+ @main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@login_required   
     
--- app/models.py
+++ app/models.py
@@ -1,13 +1,24 @@
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
-    
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    
     pass_secure = db.Column(db.String(255))
+    
     
     @property
     def password(self):
