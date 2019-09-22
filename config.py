49484cf0043afd13f3ea607874f666e92e6e9fb0
nicky-code <aline.nicole7@gmail.com>
2019-09-23 01:30:15 +0200

    created a function to upload photos.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nicky:santa7@localhost/pitch-app'
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
 
 class ProdConfig(Config):
