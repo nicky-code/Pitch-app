e5e2becefc4e9a6ee15a7f059a23391e2451c09a
nicky-code <aline.nicole7@gmail.com>
2019-09-22 22:26:15 +0200

    just installed the flask login.

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 def create_app(config_name):
 
     app = Flask(__name__)
-
+    
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
@@ -18,8 +18,13 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
 
 
     return app
